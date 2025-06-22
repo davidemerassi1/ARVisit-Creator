9b364d347eec1f9edaa4ce17394de3d39003873a
davide <davide.merassi@studenti.unimi.it>
2025-06-22 16:50:31 +0200

    rimosso appdelegate

---
+++ MuseoAccessibile Creator/MuseoAccessibileCreatorApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MuseoAccessibile Creator
+//
+//  Created by Davide Merassi on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MuseoAccessibileCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RoomSelectorView()
+        }
+    }
+}
+
